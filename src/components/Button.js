fab90bf3e93b65bb38ba45f9212741353a4f6eb5
Abby Christens <abby.christens@cbc.ca>
2019-02-07 13:52:38 -0500

    Adding all components

---
+++ src/components/Button.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+
+class Button extends Component {
+  render() {
+    return <button className="ui primary">Submit</button>
+  }
+}
+
+export default Button;
